2ed6ceff5209565604ce24fc5a960d66227e24a6
ABGEO <takalandze2000@mail.ru>
2019-01-14 17:31:45 +0400

    Added Request() function

--- functions/requests.php
+++ functions/requests.php
@@ -1,1 +1,48 @@
 <?php
+
+function Request()
+{
+    $response = array(
+        'action' => null
+    );
+
+    if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'DELETE') {
+        if (isset($_SERVER['PATH_INFO'])) {
+            $movieTitle = substr($_SERVER['PATH_INFO'], 8);
+
+            $response['action'] = 'delete';
+            $response['movie_title'] = $movieTitle;
+        }
+    } else {
+        if (isset($_SERVER['PATH_INFO'])) {
+            if ($_SERVER['PATH_INFO'] == '/movies') {
+                $response['action'] = 'get_movies';
+            } else if (strpos($_SERVER['PATH_INFO'], '/movies/') !== false) {
+                $movieTitle = substr($_SERVER['PATH_INFO'], 8);
+
+                $response['action'] = 'get_movie';
+                $response['movie_title'] = $movieTitle;
+            }
+        }
+    }
+
+    if (isset($_POST['movies'])) {
+        if (isset($_POST['title']) && isset($_POST['genre']) && isset($_POST['release_date']) && isset($_POST['imdb_rating'])) {
+            $data = array(
+                "title" => $_POST['title'],
+                "genre" => $_POST['genre'],
+                "release_date" => $_POST['release_date'],
+                "imdb_rating" => $_POST['imdb_rating']
+            );
+
+            $response['action'] = 'add_movie';
+            $response['movie_info_validation'] = true;
+            $response['movie_info'] = $data;
+        } else {
+            $response['action'] = 'add_movie';
+            $response['movie_info_validation'] = false;
+        }
+    }
+
+    return $response;
+}
--- index.php
+++ index.php
@@ -5,52 +5,37 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once('functions/movies.php');
+require_once('functions/requests.php');
 
-if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'DELETE') {
+$request = Request();
+
+if ($request['action'] == 'get_movies') {
+    //Get all movies
+
+    //header('Content-type: application/json, text/yaml');
+    header('Content-type: application/json');
+
+    echo GetMovie();
+} else if ($request['action'] == 'get_movie') {
+    //Get single movie
+    $movieTitle = $request['movie_title'];
+    $response = GetMovie($movieTitle);
+
+    if ($response == '404')
+        echo "Error 404: Movie Not Found!";
+    else {
+        header('Content-type: application/json');
+        echo $response;
+    }
+} else if ($request['action'] == 'delete') {
     //Delete movie
+    $movieTitle = $request['movie_title'];
 
-    if (isset($_SERVER['PATH_INFO'])) {
-        $movieTitle = substr($_SERVER['PATH_INFO'], 8);
-
-        echo RemoveMovie($movieTitle);
-    }
-} else {
-    //Show movies
-    if (isset($_SERVER['PATH_INFO'])) {
-        if ($_SERVER['PATH_INFO'] == '/movies') {
-            //Get all movies
-
-            //header('Content-type: application/json, text/yaml');
-            header('Content-type: application/json');
-
-            echo GetMovie();
-        } else if (strpos($_SERVER['PATH_INFO'], '/movies/') !== false) {
-            //Get single movie
-            $movieTitle = substr($_SERVER['PATH_INFO'], 8);
-            $response = GetMovie($movieTitle);
-
-            if ($response == '404')
-                echo "Error 404: Movie Not Found!";
-            else {
-                header('Content-type: application/json');
-                echo $response;
-            }
-        }
-    }
-}
-
-//Add new movie
-if (isset($_POST['movies'])) {
-    if (isset($_POST['title']) && isset($_POST['genre']) &&
-        isset($_POST['release_date']) && isset($_POST['imdb_rating'])) {
-        $data = array(
-            "title" => $_POST['title'],
-            "genre" => $_POST['genre'],
-            "release_date" => $_POST['release_date'],
-            "imdb_rating" => $_POST['imdb_rating']
-        );
-
-        echo AddMovie($data);
-    } else
+    echo RemoveMovie($movieTitle);
+} else if ($request['action'] == 'add_movie') {
+    //Add new movie
+    if ($request['movie_info_validation'])
+        echo AddMovie($request['movie_info']);
+    else
         echo "Invalid data!";
 }
